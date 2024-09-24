0974bdaf240122bfa41189c99feac21cbe7afe8f
Magnus Rolin <magnus.thor@live.dk>
2024-09-24 22:47:02 +0200

    workflow added more todos too

--- src/main/java/dk/arasbuilds/jobactions/events/listeners/CompletedOrderVaultGUIListener.java
+++ src/main/java/dk/arasbuilds/jobactions/events/listeners/CompletedOrderVaultGUIListener.java
@@ -1,9 +1,6 @@
 package dk.arasbuilds.jobactions.events.listeners;
 
-import dk.arasbuilds.jobactions.JobActions;
-import dk.arasbuilds.jobactions.database.JobActionsDatabase;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,12 +8,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class CompletedOrderVaultGUIListener implements Listener {
 
@@ -49,6 +42,7 @@ public class CompletedOrderVaultGUIListener implements Listener {
                     case 9 * 4 - 6:
                         // Implement or call the previous page logic here
                         player.sendMessage(ChatColor.YELLOW + "Previous page functionality not yet implemented.");
+                        //TODO: ADD Vault scroll (previous)
                         event.setCancelled(true); // Cancel event by default
                         break;
 
@@ -59,6 +53,7 @@ public class CompletedOrderVaultGUIListener implements Listener {
                     case 9 * 4 - 4:
                         // Implement or call the next page logic here
                         player.sendMessage(ChatColor.YELLOW + "Next page functionality not yet implemented.");
+                        //TODO: ADD Vault scroll (Next)
                         event.setCancelled(true); // Cancel event by default
                         break;
 

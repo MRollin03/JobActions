b502c2cfa4bdb165b311f2c8617f6876e81f911b
Magnus Rolin <magnus.thor@live.dk>
2024-09-27 22:32:20 +0200

    Fixed Close button in jobmarket

--- src/main/java/dk/arasbuilds/jobactions/events/listeners/MarketGUIListener.java
+++ src/main/java/dk/arasbuilds/jobactions/events/listeners/MarketGUIListener.java
@@ -20,40 +20,47 @@ public class MarketGUIListener implements Listener {
         if (event.getView().getTitle().equals(ChatColor.RED + "Job Market")) {
             event.setCancelled(true); // Cancel event by default
 
-            ItemStack clickedItem = event.getCurrentItem();
-            if (clickedItem == null || !clickedItem.hasItemMeta()) {
-                return; // Cancelled event if item is null or has no meta
-            }
-
-            ItemMeta itemMeta = clickedItem.getItemMeta();
-            if (!itemMeta.hasLore()) {
-                return; // Cancelled event if item has no lore
-            }
-
-            List<String> lore = itemMeta.getLore();
-            if (lore == null || lore.isEmpty()) {
-                return; // Cancelled event if lore is null or empty
-            }
-
             int slot = event.getSlot();
             Player player = (Player) event.getWhoClicked();
 
             switch (slot) {
-                case 48:
+
+                case 9 * 6 - 6:
                     // Implement or call the previous page logic here
                     player.sendMessage(ChatColor.YELLOW + "Previous page functionality not yet implemented.");
+                    //TODO: ADD Vault scroll (previous)
+                    event.setCancelled(true); // Cancel event by default
                     break;
 
-                case 49:
+                case 9 * 6 - 5:
                     player.closeInventory();
                     break;
 
-                case 50:
+                case 9 * 6 - 4:
                     // Implement or call the next page logic here
                     player.sendMessage(ChatColor.YELLOW + "Next page functionality not yet implemented.");
+                    //TODO: ADD Vault scroll (Next)
+                    event.setCancelled(true); // Cancel event by default
                     break;
 
                 default:
+
+
+                    ItemStack clickedItem = event.getCurrentItem();
+                    if (clickedItem == null || !clickedItem.hasItemMeta()) {
+                        return; // Cancelled event if item is null or has no meta
+                    }
+
+                    ItemMeta itemMeta = clickedItem.getItemMeta();
+                    if (!itemMeta.hasLore()) {
+                        return; // Cancelled event if item has no lore
+                    }
+
+                    List<String> lore = itemMeta.getLore();
+
+                    if (lore == null || lore.isEmpty()) {
+                        return; // Cancelled event if lore is null or empty
+                    }
                     String id = lore.get(2);
                     System.out.println("this is the id" + id);
                     ItemOrder order = JobActions.getInstance().getJobActionsDatabase().getOrderById(id);
